cf0808712ca74c69e709803062c6ba348b88822e
Aditya Nur Iskandar <43404691+Adityanuriskandar@users.noreply.github.com>
2018-10-28 21:37:21 +0700

    Add files via upload

---
+++ PBO2-10117070-Latihan53-Rabbit/src/pbo2/pkg10117070/latihan53/rabbit/PBO210117070Latihan53Rabbit.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117070.latihan53.rabbit;
+
+/**
+ *
+ * @author Aditya
+ * NAMA                 : Aditya Nur Iskandar
+ * KELAS                : PBO2
+ * NIM                  : 10117070
+ * Deskripsi Program    : Program ini berisi program untuk menampilkan data 
+ * kelinci
+ */
+public class PBO210117070Latihan53Rabbit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Rabbit r = new Rabbit("Peter",false,"grass",4,"grey");
+        r.isVegetarian();
+        r.getEats();
+        r.getNoOfLegs();
+        
+        System.out.println("Hello, his name is " + r.getName());
+        System.out.println(r.getName() + " is Vegetarian? " + r.isVegetarian());
+        System.out.println(r.getName() + " eats " + r.getEats());
+        System.out.println(r.getName() + " color is " + r.getColor());
+    
+    }
+    
+}
